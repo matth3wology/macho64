c60621c34c0cbc57c4b6b19f774eae4d6e64ed6f
Matthew Sneed <msneed@sitchai.com>
2019-05-09 23:51:51 -0400

    basic sys_write

---
+++ Makefile
@@ -0,0 +1,10 @@
+all:
+	nasm -f macho64 main.asm && ld -macosx_version_min 10.7.0 main.o -o main
+
+
+run:
+	./main.o
+
+
+clean:
+	rm main *.o
--- main.asm
+++ main.asm
@@ -1,21 +1,22 @@
-
-
-section .text
+SYS_WRITE equ 0x2000004
+SYS_EXIT equ 0x2000001
 
 global start
 
+section .text
 start:
-    mov rax, 0x2000004
+    mov rax, SYS_WRITE
     mov rdi, 1
-    mov rdx, msg, 
-    mov rcx, 13
+    mov rsi, msg, 
+    mov rdx, 13
     syscall
 
-    mov rax, 0x2000001
+    mov rax, SYS_EXIT
     mov rdi, 0
     syscall
 
 section .data
 
     msg db 'Connecting..',10
+
 
