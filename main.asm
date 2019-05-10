7e4e38eeb9696abcd25eade1104a49f5cb8545bb
Matthew Sneed <msneed@sitchai.com>
2019-05-10 01:59:53 -0400

    arguments

--- main.asm
+++ main.asm
@@ -13,35 +13,54 @@ global start
 section .text
 start:
 
-    xor rbx,rbx
-    call l1
+    ;;Check and count arguments.
+    pop r10
+    cmp r10, 2
+    jl error_message
 
-    call end_message
+    add rsp, 8
+    pop rsi
+    call str_to_int
+
+    mov r10, rax
+    cmp r10, 4
+    je correct_message
+
     exit
 
-l1:
-    call loop_message
-    inc rbx
-    cmp rbx, 3
-    jle l1
-    ret
-
-loop_message:
+error_message:
     mov rax, SYS_WRITE
     mov rdi, 1
-    mov rsi, loop_msg
-    mov rdx, 10
+    mov rsi, error
+    mov rdx, 18
     syscall
-    ret
+    exit
 
-end_message:
+correct_message:
     mov rax, SYS_WRITE
     mov rdi, 1
-    mov rsi, end_loop
-    mov rdx, 9
+    mov rsi, correct
+    mov rdx, 18
     syscall
-    ret
+    exit
+
+str_to_int:
+        xor rax, rax
+        mov rcx,  10
+next:
+	    cmp [rsi], byte 0
+	    je return_str
+	    mov bl, [rsi]
+            sub bl, 48
+	    mul rcx
+	    add rax, rbx
+	    inc rsi
+	    jmp next
+
+return_str:
+	    ret
+
 
 section .data
-    loop_msg db 'Looping..',10
-    end_loop db 'Exiting.',10
+    error db "Need an argument.",10
+    correct db "Correct argument!",10
